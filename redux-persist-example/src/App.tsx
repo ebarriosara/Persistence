f73d5fe6dd8bdc97f2168c1c0667ae88d9dc19ba
ebarriosara <estefania.barrios.aragon@ericsson.com>
2021-07-20 11:10:08 +0200

    Add PersistGate to redux-persist-example

--- redux-persist-example/src/App.tsx
+++ redux-persist-example/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Page } from './pages';
 
-const App: React.StatelessComponent<{}> = (props) => {
-
+const App: React.FunctionComponent<{}> = (props) => {
   return (
     <Provider store={store}>
-      <Page>
-        {props.children}
-      </Page>
+      <PersistGate loading={null} persistor={persistor}>
+        <Page>
+          {props.children}
+        </Page>
+      </PersistGate>
     </Provider>
   );
 };
